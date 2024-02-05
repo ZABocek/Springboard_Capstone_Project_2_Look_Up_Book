c835cd03a0a55df7acd1c37dc993d5dad9da9408
Zdenek Andrew Bocek <zabocek@gmail.com>
2024-02-05 14:24:13 -0500

    modified App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginSignup from './LoginSignup';
 import Homepage from './Homepage'; // Ensure this component is correctly imported
-import SearchBooks from './SearchBooks'; // Ensure this component is correctly imported
+import SearchBooks from './search-books'; // Ensure this component is correctly imported
 
 function App() {
     const [isAuthenticated, setIsAuthenticated] = useState(false);

06d533b50081281d35a6dcf9301dae708a5d4887
Zdenek Andrew Bocek <zabocek@gmail.com>
2024-02-19 11:31:18 -0500

    modified Homepage.js so that I have only book_id and not bookId, also modified server.js so that I correctly query for book_id

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -1,106 +1,152 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
-import './Homepage.css';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
+import "./Homepage.css";
 import likeIconURL from "./like_9790408.png";
 import dislikeIconURL from "./dislike_6933384.png";
 const Homepage = () => {
-    const [selectedBooks, setSelectedBooks] = useState([]);
-    const navigate = useNavigate(); // Initialize useNavigate
+  const [selectedBooks, setSelectedBooks] = useState([]);
+  const navigate = useNavigate(); // Initialize useNavigate
 
-    useEffect(() => {
-        async function fetchBooks() {
-            try {
-                const response = await fetch('http://localhost:5000/api/tableName');
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
-                const books = await response.json();
-                
-                setSelectedBooks(books);
-                console.log(selectedBooks);
-            } catch (error) {
-                console.error('Error fetching books:', error);
-            }
+  useEffect(() => {
+    async function fetchBooks() {
+      try {
+        const response = await fetch("http://localhost:5000/api/tableName");
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
         }
-    
-        fetchBooks();
-    }, []);   
+        let books = await response.json();
 
-    // Inside the Homepage component
-    const handleLike = async (bookId, liked) => {
-        const userId = localStorage.getItem('userId');
-        console.log("Attempting to like/dislike bookId:", bookId, "Liked:", liked, "UserId:", userId); // Debugging line
-    
-        try {
-            const response = await fetch('http://localhost:5000/api/like', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ userId, bookId, liked })
-            });
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            console.log('Like/dislike successfully processed');
-        } catch (error) {
-            console.error('Error processing like/dislike:', error);
-        }
-    };
-    
+        // Transform each book to rename book_id to bookId
+        books = books.map((book) => ({
+          ...book,
+          bookId: book.book_id,
+        }));
 
-    
-    // Add this function to handle logout
-    const handleLogout = () => {
-        // Here you should clear your authentication token or session
-        localStorage.removeItem('token'); // For example, if you're using localStorage
-        navigate('/login'); // Redirect to login page
-    };
+        setSelectedBooks(books);
+      } catch (error) {
+        console.error("Error fetching books:", error);
+      }
+    }
 
-    return (
-        <div className="homepage">
-            <h1 className="welcome-message">Welcome to Look Up Book!</h1>
-            <h2 className="recommendation-message">Here are 10 books recommended for you to enjoy from award-winning authors!</h2>
-            <nav className="navbar">
-                <ul>
-                    <li><a href="/add-db-book">Add Book from Database to Profile</a></li>
-                    <li><a href="/add-new-book">Submit New Award-winning Book for Verification</a></li>
-                    <li><a href="/search-books">Search Books By Authors</a></li>
-                    <li><a href="/profile">Profile Page</a></li>
-                    <li><a href="/search-awards">Search Books by Awards</a></li>
-                    <li><a href="/delete-book">Delete Book from Profile</a></li>
-                    <li><a href="#" onClick={handleLogout}>Logout</a></li> {/* Modify this line */}
-                </ul>
-            </nav>
-            <div className="book-table">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Like/Dislike</th>
-                            <th>Genre</th>
-                            <th>Year</th>
-                            <th>Verified</th>
-                            <th>Author ID</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {selectedBooks.map((book, index) => (
-                        <tr key={index}>
-                            <td>{book.title_of_winning_book}</td>
-                            <td>
-                                <img src={likeIconURL} alt="Like" onClick={() => handleLike(book.book_id, true)} style={{ cursor: 'pointer', marginRight: '10px', height: '50px' }} />
-                                <img src={dislikeIconURL} alt="Dislike" onClick={() => handleLike(book.book_id, false)} style={{ cursor: 'pointer', height: '50px' }} />
-                                </td>
-                                <td>{book.prize_genre}</td>
-                                <td>{book.prize_year}</td>
-                                <td>{book.verified ? 'True' : 'False'}</td>
-                                <td>{book.person_id}</td>
-                                </tr>
-                                ))}
-                                </tbody>
-                                </table>
-                                </div>
-                                </div>
-                                );
-                            };
+    fetchBooks();
+  }, []);
+
+  // Inside the Homepage component
+  const handleLike = async (bookId, liked) => {
+    const userId = localStorage.getItem("userId");
+    console.log(
+      "Attempting to like/dislike bookId:",
+      bookId,
+      "Liked:",
+      liked,
+      "UserId:",
+      userId
+    ); // Debugging line
+
+    try {
+      const response = await fetch("http://localhost:5000/api/like", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ userId, bookId, liked }),
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      console.log("Like/dislike successfully processed");
+    } catch (error) {
+      console.error("Error processing like/dislike:", error);
+    }
+  };
+
+  // Add this function to handle logout
+  const handleLogout = () => {
+    // Here you should clear your authentication token or session
+    localStorage.removeItem("token"); // For example, if you're using localStorage
+    navigate("/login"); // Redirect to login page
+  };
+
+  return (
+    <div className="homepage">
+      <h1 className="welcome-message">Welcome to Look Up Book!</h1>
+      <h2 className="recommendation-message">
+        Here are 10 books recommended for you to enjoy from award-winning
+        authors!
+      </h2>
+      <nav className="navbar">
+        <ul>
+          <li>
+            <a href="/add-db-book">Add Book from Database to Profile</a>
+          </li>
+          <li>
+            <a href="/add-new-book">
+              Submit New Award-winning Book for Verification
+            </a>
+          </li>
+          <li>
+            <a href="/search-books">Search Books By Authors</a>
+          </li>
+          <li>
+            <a href="/profile">Profile Page</a>
+          </li>
+          <li>
+            <a href="/search-awards">Search Books by Awards</a>
+          </li>
+          <li>
+            <a href="/delete-book">Delete Book from Profile</a>
+          </li>
+          <li>
+            <a href="#" onClick={handleLogout}>
+              Logout
+            </a>
+          </li>{" "}
+          {/* Modify this line */}
+        </ul>
+      </nav>
+      <div className="book-table">
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Like/Dislike</th>
+              <th>Genre</th>
+              <th>Year</th>
+              <th>Verified</th>
+              <th>Author ID</th>
+            </tr>
+          </thead>
+          <tbody>
+            {selectedBooks.map((book, index) => (
+              <tr key={index}>
+                <td>{book.title_of_winning_book}</td>
+                <td>
+                  <img
+                    src={likeIconURL}
+                    alt="Like"
+                    onClick={() => handleLike(book.bookId, true)}
+                    style={{
+                      cursor: "pointer",
+                      marginRight: "10px",
+                      height: "50px",
+                    }}
+                  />
+                  <img
+                    src={dislikeIconURL}
+                    alt="Dislike"
+                    onClick={() => handleLike(book.bookdId, false)}
+                    style={{ cursor: "pointer", height: "50px" }}
+                  />
+                </td>
+                <td>{book.prize_genre}</td>
+                <td>{book.prize_year}</td>
+                <td>{book.verified ? "True" : "False"}</td>
+                <td>{book.person_id}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
 export default Homepage;
--- server/server.js
+++ server/server.js
@@ -1,170 +1,185 @@
-require('dotenv').config({ path: '.env' }); // Adjust the path to the .env file if needed
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const { Pool } = require('pg');
+require("dotenv").config({ path: ".env" }); // Adjust the path to the .env file if needed
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { Pool } = require("pg");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 // PostgreSQL connection setup
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 // JWT secret key
 const jwtSecret = process.env.JWT_SECRET;
 
-app.post('/signup', async (req, res) => {
-    const { username, password, email } = req.body;
+app.post("/signup", async (req, res) => {
+  const { username, password, email } = req.body;
 
-    if (!username || !password || !email) {
-        return res.status(400).json('Incorrect form submission');
-    }
+  if (!username || !password || !email) {
+    return res.status(400).json("Incorrect form submission");
+  }
 
-    const saltRounds = 10;
-    try {
-        const hash = await bcrypt.hash(password, saltRounds);
-        const client = await pool.connect();
-        // When inserting the user, you're already returning the user data
-        const user = await client.query('INSERT INTO users (username, email, hash) VALUES ($1, $2, $3) RETURNING *', [username, email, hash]);
+  const saltRounds = 10;
+  try {
+    const hash = await bcrypt.hash(password, saltRounds);
+    const client = await pool.connect();
+    // When inserting the user, you're already returning the user data
+    const user = await client.query(
+      "INSERT INTO users (username, email, hash) VALUES ($1, $2, $3) RETURNING *",
+      [username, email, hash]
+    );
 
-        const token = jwt.sign({ id: user.rows[0].id }, jwtSecret, { expiresIn: '2h' });
+    const token = jwt.sign({ id: user.rows[0].id }, jwtSecret, {
+      expiresIn: "2h",
+    });
 
-        // Include userId in the response
-        res.json({ token, userId: user.rows[0].id }); // Modified line
-        client.release();
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ message: 'Error registering new user', error: err.message });
-    }
+    // Include userId in the response
+    res.json({ token, userId: user.rows[0].id }); // Modified line
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ message: "Error registering new user", error: err.message });
+  }
 });
 
-app.get('/api/tableName', async (req, res) => {
-    try {
-        const client = await pool.connect();
-        
-        // SQL query to get 10 random books where role is 'winner' and title_of_winning_book is not null
-        const queryText = `
-            SELECT title_of_winning_book, prize_genre, prize_year, verified, person_id
+app.get("/api/tableName", async (req, res) => {
+  try {
+    const client = await pool.connect();
+
+    // SQL query to get 10 random books where role is 'winner' and title_of_winning_book is not null
+    const queryText = `
+            SELECT book_id, title_of_winning_book, prize_genre, prize_year, verified, person_id
             FROM tableName
             WHERE role = 'winner' AND title_of_winning_book IS NOT NULL
             ORDER BY RANDOM()
             LIMIT 10;
         `;
 
-        const result = await client.query(queryText);
+    const result = await client.query(queryText);
 
-        res.json(result.rows);
-        client.release();
-    } catch (error) {
-        console.error('Error fetching books from database:', error);
-        res.status(500).send('Error fetching books');
-    }
+    res.json(result.rows);
+    client.release();
+  } catch (error) {
+    console.error("Error fetching books from database:", error);
+    res.status(500).send("Error fetching books");
+  }
 });
 
-app.get('/api/authors', async (req, res) => {
-    try {
-        const client = await pool.connect();
-        const queryText = 'SELECT DISTINCT author_id, last_name, given_name FROM tableName WHERE author_id IS NOT NULL ORDER BY last_name, given_name;';
-        const result = await client.query(queryText);
-        res.json(result.rows);
-        client.release();
-    } catch (error) {
-        console.error('Error fetching authors:', error);
-        res.status(500).send('Error fetching authors');
-    }
+app.get("/api/authors", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const queryText =
+      "SELECT DISTINCT author_id, last_name, given_name FROM tableName WHERE author_id IS NOT NULL ORDER BY last_name, given_name;";
+    const result = await client.query(queryText);
+    res.json(result.rows);
+    client.release();
+  } catch (error) {
+    console.error("Error fetching authors:", error);
+    res.status(500).send("Error fetching authors");
+  }
 });
 
-app.get('/api/books/:authorId', async (req, res) => {
-    const { authorId } = req.params;
-    try {
-        const client = await pool.connect();
-        const queryText = `
+app.get("/api/books/:authorId", async (req, res) => {
+  const { authorId } = req.params;
+  try {
+    const client = await pool.connect();
+    const queryText = `
             SELECT book_id, title_of_winning_book, prize_genre, prize_year, verified, author_id
             FROM tableName
             WHERE author_id = $1
             ORDER BY book_id;
         `;
-        const result = await client.query(queryText, [authorId]);
-        res.json(result.rows);
-        client.release();
-    } catch (error) {
-        console.error(`Error fetching books for author ${authorId}:`, error);
-        res.status(500).send('Error fetching books');
-    }
+    const result = await client.query(queryText, [authorId]);
+    res.json(result.rows);
+    client.release();
+  } catch (error) {
+    console.error(`Error fetching books for author ${authorId}:`, error);
+    res.status(500).send("Error fetching books");
+  }
 });
 
 // Add this endpoint to server.js
-app.get('/api/awards', async (req, res) => {
-    try {
-        const client = await pool.connect();
-        const queryText = `
+app.get("/api/awards", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const queryText = `
             SELECT DISTINCT award_id, prize_name
             FROM tableName
             WHERE role = 'winner' AND prize_type = 'book' AND title_of_winning_book IS NOT NULL AND title_of_winning_book != ''
             ORDER BY award_id;
         `;
-        const result = await client.query(queryText);
-        res.json(result.rows);
-        client.release();
-    } catch (error) {
-        console.error('Error fetching awards:', error);
-        res.status(500).send('Error fetching awards');
-    }
+    const result = await client.query(queryText);
+    res.json(result.rows);
+    client.release();
+  } catch (error) {
+    console.error("Error fetching awards:", error);
+    res.status(500).send("Error fetching awards");
+  }
 });
 
-app.get('/api/awards/:awardId', async (req, res) => {
-    const { awardId } = req.params;
-    try {
-        const client = await pool.connect();
-        const queryText = `
+app.get("/api/awards/:awardId", async (req, res) => {
+  const { awardId } = req.params;
+  try {
+    const client = await pool.connect();
+    const queryText = `
             SELECT * FROM tableName
             WHERE award_id = $1 AND role = 'winner' AND prize_type = 'book' AND title_of_winning_book IS NOT NULL AND title_of_winning_book != ''
             ORDER BY prize_year;
         `;
-        const result = await client.query(queryText, [awardId]);
-        res.json(result.rows);
-        client.release();
-    } catch (error) {
-        console.error(`Error fetching books for award ${awardId}:`, error);
-        res.status(500).send('Error fetching books');
-    }
+    const result = await client.query(queryText, [awardId]);
+    res.json(result.rows);
+    client.release();
+  } catch (error) {
+    console.error(`Error fetching books for award ${awardId}:`, error);
+    res.status(500).send("Error fetching books");
+  }
 });
 
 // Endpoint to handle likes and dislikes
-app.post('/api/like', async (req, res) => {
-    const { userId, bookId, liked } = req.body; // Note: 'liked' is now a boolean indicating like or dislike
+app.post("/api/like", async (req, res) => {
+  const { userId, bookId, liked } = req.body; // Note: 'liked' is now a boolean indicating like or dislike
 
-    try {
-        const client = await pool.connect();
+  try {
+    const client = await pool.connect();
 
-        // Check if there's an existing like/dislike by the user for this book
-        const existingEntry = await client.query('SELECT * FROM user_book_likes WHERE user_id = $1 AND book_id = $2', [userId, bookId]);
+    // Check if there's an existing like/dislike by the user for this book
+    const existingEntry = await client.query(
+      "SELECT * FROM user_book_likes WHERE user_id = $1 AND book_id = $2",
+      [userId, bookId]
+    );
 
-        if (existingEntry.rows.length > 0) {
-            // Update the existing entry if user changes their like/dislike
-            await client.query('UPDATE user_book_likes SET liked = $1, likedOn = NOW() WHERE user_id = $2 AND book_id = $3', [liked, userId, bookId]);
-        } else {
-            // Insert new like/dislike
-            await client.query('INSERT INTO user_book_likes (user_id, book_id, liked, likedOn) VALUES ($1, $2, $3, NOW())', [userId, bookId, liked]);
-        }
-        res.json({ message: 'Success' });
-        client.release();
-    } catch (error) {
-        console.error('Error processing like/dislike', error);
-        res.status(500).send('Error processing like/dislike');
+    if (existingEntry.rows.length > 0) {
+      // Update the existing entry if user changes their like/dislike
+      await client.query(
+        "UPDATE user_book_likes SET liked = $1, likedOn = NOW() WHERE user_id = $2 AND book_id = $3",
+        [liked, userId, bookId]
+      );
+    } else {
+      // Insert new like/dislike
+      await client.query(
+        "INSERT INTO user_book_likes (user_id, book_id, liked, likedOn) VALUES ($1, $2, $3, NOW())",
+        [userId, bookId, liked]
+      );
     }
+    res.json({ message: "Success" });
+    client.release();
+  } catch (error) {
+    console.error("Error processing like/dislike", error);
+    res.status(500).send("Error processing like/dislike");
+  }
 });
 
-
-
 app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+  console.log("Server is running on port 5000");
 });

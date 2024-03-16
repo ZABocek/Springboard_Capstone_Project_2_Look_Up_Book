32fda7eeb09edb387cf197c71c97324a393740ac
Zdenek Andrew Bocek <zabocek@gmail.com>
2024-03-16 17:14:03 +0100

    modified add-new-book.js

--- client/src/add-new-book.js
+++ client/src/add-new-book.js
@@ -55,9 +55,20 @@ const AddNewBook = () => {
             <form onSubmit={handleSubmit}>
                 <label>Author's Full Name:</label>
                 <input type="text" name="fullName" value={bookDetails.fullName} onChange={handleChange} required />
-
-                {/* Repeat the above pattern for all other fields */}
-                
+                <label>Author's Given Name:</label>
+                <input type="text" name="givenName" value={bookDetails.givenName} onChange={handleChange} required />
+                <label>Author's Last Name:</label>
+                <input type="text" name="lastName" value={bookDetails.lastName} onChange={handleChange} required />
+                <label>Exceptional University Author Went To (If They Went To An Elite Institution):</label>
+                <input type="text" name="eliteInstitution" value={bookDetails.eliteInstitution} onChange={handleChange} />
+                <label>Graduate Degree? (please fill with word "graduate" if so):</label>
+                <input type="text" name="graduateDegree" value={bookDetails.graduateDegree} onChange={handleChange} />
+                <label>Master of Fine Arts Degree? (please enter where they got their Master's, if so):</label>
+                <input type="text" name="mfaDegree" value={bookDetails.mfaDegree} onChange={handleChange} />
+                <label>Name of Prize Book Won (Please Start Each Word With A Capital Letter):</label>
+                <input type="text" name="prizeName" value={bookDetails.prizeName} onChange={handleChange} required />
+                <label>Year Book Won The Award:</label>
+                <input type="integer" name="prizeYear" value={bookDetails.prizeYear} onChange={handleChange} required />
                 <label>Prize Genre (Prose or Poetry):</label>
                 <select name="prizeGenre" value={bookDetails.prizeGenre} onChange={handleChange} required>
                     <option value="">Select Genre</option>
